95573fbf0caa545e162fbecd5c7642d8e0499899
Matias  Benincasa <matiasbenincasa@PoniBook.local>
2020-06-16 20:04:58 +0200

    feat: add Button component

---
+++ components/Button/Button.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import colors from "../../config/colors";
+
+const AppButton = (props) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: props.color }]}
+      onPress={props.onPress}
+    >
+      <Text style={styles.text}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+    marginBottom: 25,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
+
+export default AppButton;
