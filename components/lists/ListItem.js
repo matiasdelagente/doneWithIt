54541ecbf9271cc66402d37656859524707b52c5
Matias  Benincasa <matiasbenincasa@PoniBook.local>
2020-08-06 12:12:38 +0200

    add list components

---
+++ components/lists/ListItem.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Text from "../Text";
+import colors from "../../config/colors";
+
+const ListItem = ({
+  title,
+  subTitle,
+  image,
+  onPress,
+  renderRightActions,
+  IconComponent,
+}) => {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
+        <View style={styles.listContainer}>
+          {IconComponent}
+          {image && <Image source={image} style={styles.image} />}
+          <View style={styles.detailsContainer}>
+            <Text style={styles.title} numberOfLines={1}>
+              {title}
+            </Text>
+            {subTitle && (
+              <Text style={styles.subTitle} numberOfLines={2}>
+                {subTitle}
+              </Text>
+            )}
+          </View>
+          <MaterialCommunityIcons name="chevron-right" size={25} color={colors.medium} />
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
+    alignItems: "center",
+  },
+  image: {
+    height: 70,
+    width: 70,
+    borderRadius: 35,
+  },
+  detailsContainer: {
+    flex: 1,
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "500",
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+});
+
+export default ListItem;
